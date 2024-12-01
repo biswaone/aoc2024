8d5abb4c8acdaf0ef501e48d6a497d80b6030df6
Biswa Prakash Mishra <biswaprakash.mishra@infinitylearn.com>
2024-12-02 03:10:20 +0530

    improvement

--- day1/main.rs
+++ day1/main.rs
@@ -1,42 +1,59 @@
+use std::fs::read_to_string;
 use std::collections::HashMap;
 
+fn part1(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
+    left.iter()
+        .zip(right.iter())
+        .map(|(a, b)| (a - b).abs())
+        .sum()
+}
 
-fn solve() {
-    let contents = std::fs::read_to_string("input.txt").expect("Failed to read file");
-    let mut locations1 = Vec::new();
-    let mut locations2 = Vec::new();
+fn part2(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
     let mut frequency_map = HashMap::new();
+    let mut similarity_score = 0;
 
-    for line in contents.lines() {
-        let locations: Vec<i32> = line.split_whitespace()
-            .filter_map(|s| s.parse::<i32>().ok())
-            .collect();
-        locations1.push(locations[0]);
-        locations2.push(locations[1]);
-    }
-    locations1.sort();
-    locations2.sort();
-    let sum: i32 = locations1.iter()
-    .zip(locations2.iter())
-    .map(|(a, b)| (a - b).abs())
-    .sum();
-    println!("{:?}", sum);
-
-    for &loc in &locations2 {
+    for &loc in right.iter() {
         *frequency_map.entry(loc).or_insert(0) += 1;
     }
 
-   let mut similarity_score = 0;
-    for loc in locations1 {
-        match frequency_map.get(&loc) {
+    for loc in left.iter() {
+        match frequency_map.get(loc) {
             Some(frequency) => similarity_score += loc * frequency,
-            None => similarity_score += 0,
+            None => {} 
         }
     }
-   println!("{:?}", similarity_score);
+    similarity_score
+}
 
+
+fn input() -> (Vec<i32>, Vec<i32>) {
+    let content = read_to_string("input.txt").unwrap();
+
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    for line in content.lines() {
+        let locations: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse::<i32>().ok())
+            .collect();
+
+        if locations.len() == 2 {
+            left.push(locations[0]);
+            right.push(locations[1]);
+        }
+    }
+
+    left.sort();
+    right.sort();
+
+    (left, right)
 }
 
 fn main() {
-    solve()
+    let (left, right) = input();
+    let sol1 = part1(&left, &right);
+    let sol2 = part2(&left, &right);
+    println!("Part A: {:?}", sol1);
+    println!("Part B: {:?}", sol2);
 }
